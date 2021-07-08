df059e1fd362af191e672423f163693f26fd98c4
Pedro P. Medina <pedrodelilia@gmail.com>
2021-07-08 11:47:55 -0400

    Return hex table instead

--- lua/darkside/colors.lua
+++ lua/darkside/colors.lua
@@ -1,19 +1,34 @@
-local lush = require('lush')
-local hsl = lush.hsl
-
+-- local lush = require('lush')
+-- local hsl = lush.hsl
+-- return {
+--     white = hsl(219, 14, 71), -- #ABB2BF
+--     light_gray = hsl(219, 13, 34), -- #4A5261
+--     gray = hsl(220, 14, 25), -- #363C48
+--     dark_gray = hsl(222, 14, 18), -- #272B34
+--     black = hsl(220, 17, 14), -- #1e222a
+--     dark_black = hsl(220, 18, 13), -- #1b1f27
+--     red = hsl(354, 50, 66), -- #D27E84
+--     dark_red = hsl(5, 48, 51), -- #BE5046
+--     green = hsl(92, 28, 65), -- #A3BE8C
+--     yellow = hsl(40, 67, 72), -- #E7C788
+--     dark_yellow = hsl(29, 54, 61), -- #D19A66
+--     blue = hsl(207, 82, 66), -- #61AFEF
+--     purple = hsl(282, 68, 71), -- #C882E7
+--     cyan = hsl(223, 70, 79) -- #A3B8EF
+-- }
 return {
-    white = hsl(219, 14, 71), -- #ABB2BF
-    light_gray = hsl(219, 13, 34), -- #4A5261
-    gray = hsl(220, 14, 25), -- #363C48
-    dark_gray = hsl(222, 14, 18), -- #272B34
-    black = hsl(220, 17, 14), -- #1e222a
-    dark_black = hsl(220, 18, 13), -- #1b1f27
-    red = hsl(354, 50, 66), -- #D27E84
-    dark_red = hsl(5, 48, 51), -- #BE5046
-    green = hsl(92, 28, 65), -- #A3BE8C
-    yellow = hsl(40, 67, 72), -- #E7C788
-    dark_yellow = hsl(29, 54, 61), -- #D19A66
-    blue = hsl(207, 82, 66), -- #61AFEF
-    purple = hsl(282, 68, 71), -- #C882E7
-    cyan = hsl(223, 70, 79) -- #A3B8EF
+    white = '#ABB2BF',
+    light_gray = '#4A5261',
+    gray = '#363C48',
+    dark_gray = '#272B34',
+    black = '#1e222a',
+    dark_black = '#1b1f27',
+    red = '#D27E84',
+    dark_red = '#BE5046',
+    green = '#A3BE8C',
+    yellow = '#E7C788',
+    dark_yellow = '#D19A66',
+    blue = '#61AFEF',
+    purple = '#C882E7',
+    cyan = '#A3B8EF'
 }
